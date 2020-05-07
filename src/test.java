d54bb04c4bd75282240e4dbacccd19238c1ccdbf
mohammad^C <mohammad100003@hotmail.com>
2020-05-08 01:24:21 +0300

    deleting Graph class and adding Senario class

--- src/test.java
+++ src/test.java
@@ -2,8 +2,8 @@ import java.util.LinkedList;
 
 public class test {
     public static void main(String[] args) {
-        Graph g=new Graph(5);
-        g.display();
+        Senario s=new Senario();
+        s.display();
 
     }
 }
